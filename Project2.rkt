d0a3642bc0c6307e57248f888714960eabcbe594
Eric Foy <foy.8@wright.edu>
2022-09-20 20:06:21 -0400

    replace nested define with let

--- Project2.rkt
+++ Project2.rkt
@@ -1,48 +1,39 @@
-(define set-equal?
-    (lambda (set1 set2)
-      (let loop1 ((set1sub set1))
-        (if (null? set1sub) #t
-            (let loop2 ((set2sub set2))
-              (if (null? set2sub) #f
-                  (if (= (car set1sub) (car set2sub)) (loop1 (cdr set1sub))
-                      (loop2 (cdr set2sub)))))))))
+#lang racket
+; Eric Foy
 
 (define set-equal?
     (lambda (set1 set2)
       (letrec ((loop1 (lambda (set1sub set2big)
                  (if (null? set1sub) #t
-		   (loop2 set1sub set2big set2big))))
+                   (loop2 set1sub set2big set2big))))
                (loop2 (lambda (set1sub set2sub set2big)
                  (if (null? set2sub) #f
                    (if (= (car set1sub) (car set2sub))
-		     (loop1 (cdr set1sub) set2big)
+                     (loop1 (cdr set1sub) set2big)
                      (loop2 set1sub (cdr set2sub) set2big))))))
-              (begin
-		(define a (loop1 set1 set2))
-	        (define b (loop1 set2 set1))
-	        (if (eqv? a b) a #f)))))
+              (let ((a (loop1 set1 set2))
+                    (b (loop1 set2 set1)))
+                (if (eqv? a b) a #f)))))
 
 (define nested-set-equal?
     (lambda (set1 set2)
       (letrec ((loop1 (lambda (set1sub set2big)
                  (if (null? set1sub) #t
-		   (loop2 set1sub set2big set2big))))
+                   (loop2 set1sub set2big set2big))))
                (loop2 (lambda (set1sub set2sub set2big)
                  (if (null? set2sub) #f
                    (if (number? (car set1sub))
                      (if (number? (car set2sub))
                        (if (= (car set1sub) (car set2sub))
                          (loop1 (cdr set1sub) set2big)
-	                 (loop2 set1sub (cdr set2sub) set2big))
-	               (loop2 set1sub (cdr set2sub) set2big))
+                         (loop2 set1sub (cdr set2sub) set2big))
+                       (loop2 set1sub (cdr set2sub) set2big))
                      (if (number? (car set2sub))
-	               (loop2 set1sub (cdr set2sub) set2big)
-		       (begin
-			 (define c (loop1 (car set1sub) (car set2sub)))
-			 (define d (loop1 (car set2sub) (car set1sub)))
-			 (if (eqv? c d) c #f))))))))
-              (begin
-		(define a (loop1 set1 set2))
-	        (define b (loop1 set2 set1))
-	        (if (eqv? a b) a #f)))))
+                       (loop2 set1sub (cdr set2sub) set2big)
+                       (let ((c (loop1 (car set1sub) (car set2sub)))
+                             (d (loop1 (car set2sub) (car set1sub))))
+                         (if (eqv? c d) c #f))))))))
+              (let ((a (loop1 set1 set2))
+                    (b (loop1 set2 set1)))
+                (if (eqv? a b) a #f)))))
 
