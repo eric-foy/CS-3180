b951860a2fc3772b79dfb9ba92145150aa0ed75f
Eric Foy <foy.8@wright.edu>
2022-09-23 21:07:18 -0400

    union tests

--- Project2Tests.rkt
+++ Project2Tests.rkt
@@ -276,3 +276,106 @@
 (nested-set-equal? '(3 (1 2 (9 8)) 4 5) '(3 (1 2 (9 8)) 4 5))
 (display " = #t")
 (newline)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; union
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(display "from handout") (newline)
+(union '(1 2 3 4) '(2 3 4 5))
+(display " = (1 2 3 4 5)") (newline)
+
+(display "equal length, no dups, sorted, not sparce") (newline)
+(union '(1 2 3 4) '(5 6 7 8))
+(display " = (1 2 3 4 5 6 7 8") (newline)
+(union '(5 6 7 8) '(1 2 3 4))
+(display " = (1 2 3 4 5 6 7 8") (newline)
+
+(display "equal length, no dups, sorted, sparce") (newline)
+(union '(1 4 6 7) '(2 3 5 8))
+(display " = (1 2 3 4 5 6 7 8") (newline)
+(union '(2 3 5 8) '(1 4 6 7))
+(display " = (1 2 3 4 5 6 7 8") (newline)
+
+(display "equal length, no dups, not sorted, not sparce") (newline)
+(union '(3 2 1 4) '(7 8 5 6))
+(display " = (1 2 3 4 5 6 7 8") (newline)
+(union '(7 8 5 6) '(3 2 1 4))
+(display " = (1 2 3 4 5 6 7 8") (newline)
+
+(display "equal length, no dups, not sorted, sparce") (newline)
+(union '(8 3 2 5) '(1 4 7 6))
+(display " = (1 2 3 4 5 6 7 8") (newline)
+(union '(1 4 7 6) '(8 3 2 5))
+(display " = (1 2 3 4 5 6 7 8") (newline)
+
+(display "equal length, dups, sorted, not sparce") (newline)
+(union '(1 2 2 3) '(4 4 5 6))
+(display " = (1 2 3 4 5 6") (newline)
+(union '(4 4 5 6) '(1 2 2 3))
+(display " = (1 2 3 4 5 6") (newline)
+
+(display "equal length, dups, sorted, sparce") (newline)
+(union '(1 4 5 5) '(2 3 3 6))
+(display " = (1 2 3 4 5 6") (newline)
+(union '(2 3 3 6) '(1 4 5 5))
+(display " = (1 2 3 4 5 6") (newline)
+
+(display "equal length, dups, not sorted, not sparce") (newline)
+(union '(3 2 1 2) '(5 5 6 4))
+(display " = (1 2 3 4 5 6") (newline)
+(union '(5 5 6 4) '(3 2 1 2))
+(display " = (1 2 3 4 5 6") (newline)
+
+(display "equal length, dups, not sorted, sparce") (newline)
+(union '(5 1 4 5) '(3 2 3 6))
+(display " = (1 2 3 4 5 6") (newline)
+(union '(3 2 3 6) '(5 1 4 5))
+(display " = (1 2 3 4 5 6") (newline)
+
+(display "not equal length, no dups, sorted, not sparce") (newline)
+(union '(1 2 3) '(4 5 6 7 8 9))
+(display " = (1 2 3 4 5 6 7 8 9") (newline)
+(union '(4 5 6 7 8 9) '(1 2 3))
+(display " = (1 2 3 4 5 6 7 8 9") (newline)
+
+(display "not equal length, no dups, sorted, sparce") (newline)
+(union '(1 5 6) '(2 3 4 7 8 9))
+(display " = (1 2 3 4 5 6 7 8 9") (newline)
+(union '(2 3 4 7 8 9) '(1 5 6))
+(display " = (1 2 3 4 5 6 7 8 9") (newline)
+
+(display "not equal length, no dups, not sorted, not sparce") (newline)
+(union '(3 1 2) '(7 8 4 6 5 9))
+(display " = (1 2 3 4 5 6 7 8 9") (newline)
+(union '(7 8 4 6 5 9) '(3 1 2))
+(display " = (1 2 3 4 5 6 7 8 9") (newline)
+
+(display "not equal length, no dups, not sorted, sparce") (newline)
+(union '(5 6 1) '(4 2 7 3 9 8))
+(display " = (1 2 3 4 5 6 7 8 9") (newline)
+(union '(4 2 7 3 9 8) '(5 6 1))
+(display " = (1 2 3 4 5 6 7 8 9") (newline)
+
+(display "not equal length, dups, sorted, not sparce") (newline)
+(union '(1 2 2) '(3 4 5 6 6 7))
+(display " = (1 2 3 4 5 6 7") (newline)
+(union '(3 4 5 6 6 7) '(1 2 2))
+(display " = (1 2 3 4 5 6 7") (newline)
+
+(display "not equal length, dups, sorted, sparce") (newline)
+(union '(1 4 4) '(2 3 5 5 6 7))
+(display " = (1 2 3 4 5 6 7") (newline)
+(union '(2 3 5 5 6 7) '(1 4 4))
+(display " = (1 2 3 4 5 6 7") (newline)
+
+(display "not equal length, dups, not sorted, not sparce") (newline)
+(union '(2 1 2) '(4 3 6 6 5 7))
+(display " = (1 2 3 4 5 6 7") (newline)
+(union '(4 3 6 6 5 7) '(2 1 2))
+(display " = (1 2 3 4 5 6 7") (newline)
+
+(display "not equal length, dups, not sorted, sparce") (newline)
+(union '(4 1 4) '(3 2 5 5 7 6))
+(display " = (1 2 3 4 5 6 7") (newline)
+(union '(3 2 5 5 7 6) '(4 1 4))
+(display " = (1 2 3 4 5 6 7") (newline)
