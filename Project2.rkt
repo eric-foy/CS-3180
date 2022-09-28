a32de6bf90ba327d3423b58a310bc7f692ae319b
Eric Foy <foy.8@wright.edu>
2022-09-28 15:10:19 -0400

    added function comments

--- Project2.rkt
+++ Project2.rkt
@@ -1,6 +1,13 @@
 #lang racket
 ; Eric Foy
 
+; set-equal? - Expects two lists, each of which represents a set of integers
+; Two sets are equal if they contain the same members, ignoring the order.
+; Assuming that the sets contain no duplicate values, and no recursive subsets.
+; Returns: (boolean) #t if the sets are equal, #f otherwise.
+; Parameters:
+; set1 (list) - set of integers
+; set2 (list) - set of integers
 (define set-equal?
     (lambda (set1 set2)
       (letrec ((loop1 (lambda (set1sub set2big)
@@ -15,6 +22,14 @@
                     (b (loop1 set2 set1)))
                 (if (eqv? a b) a #f)))))
 
+; nested-set-equal? - Identical to set-equal, but the sets can contain other
+; sets. Sets made up of numbers and lists of numbers, So assuming if an element
+; of a set is not a number then it is a list.
+; Returns: (boolean) #t if the sets, containing nested sets, are equal, #f
+; otherwise.
+; Parameters:
+; set1 (list) - set containing integers and sets of integers
+; set2 (list) - set containing integers and sets of integers
 (define nested-set-equal?
     (lambda (set1 set2)
       (letrec ((loop1 (lambda (set1sub set2big)
@@ -41,6 +56,13 @@
                     (b (loop1 set2 set1)))
                 (if (eqv? a b) a #f)))))
 
+; union - Expects two lists, each of which represents a set of integers.  The
+; union of two sets contains every element from either of the two sets, with no
+; repetitions.
+; Returns: (list) The union of the two sets.
+; Parameters:
+; set1 (list) - set of integers
+; set2 (list) - set of integers
 (define union
   (lambda (set1 set2)
     (letrec ((loop (lambda (set1 o)
@@ -59,6 +81,13 @@
         (if (equal? set2 '()) set1
           (loop set1 set2))))))
 
+; intersection - Expects two lists, each of which represents a set of integers.
+; The intersection of two sets contains every element that is present in both
+; sets, with no repetitions
+; Returns: (list) The intersection of the two sets.
+; Parameters:
+; set1 (list) - set of integers
+; set2 (list) - set of integers
 (define intersection
   (lambda (set1 set2)
     (letrec ((main (lambda (a b)
@@ -87,6 +116,12 @@
         (if (null? set2) '()
           (main set1 set2))))))
 
+; mergesort - Expects a flat (no sub-lists) list of integers. Sorts the
+; integers using the merge sort algorithm and returns the resulting sorted
+; list.
+; Returns: (list) The resulting sorted list.
+; Parameters:
+; lst (list) - flat list of integers
 (define mergesort
   (lambda (lst)
     (letrec ((main (lambda (l)
@@ -121,6 +156,11 @@
       (if (null? lst) '()
         (main lst)))))
 
+; powerset - Expects a flat (no sub-lists) list representing a set of of
+; integers. Returns a list of lists representing the powerset of the input set
+; Returns: (list) List of lists representing the powerset of the input set.
+; Parameters:
+; lst (list) - flat list of integers
 (define powerset
   (lambda (lst)
     (letrec ((main (lambda (l o)
@@ -136,8 +176,13 @@
                              (cons (cons a (car l)) o))))))
       (main lst '()))))
 
-; lst made up of integers and lists of integers.
-; So assuming if not list then integer.
+; nested-reduce - Expects a list of integers, which may include sublists
+; (nested lists). Returns a version of the list that has no duplicate integers,
+; and no duplicate sublists. Order matters. lst made up of integers and lists
+; of integers, So assuming if not list then integer.
+; Returns: (list) List with no duplicate integers and no duplicate sublists.
+; Parameters:
+; lst (list) - ordered list of integers and lists of integers
 (define nested-reduce
   (lambda (lst)
     (letrec ((main (lambda (l o)
