8466a1e750cdc830d05a1f9094b84f3aad178377
Eric Foy <foy.8@wright.edu>
2022-10-23 13:24:45 -0400

    working list?

--- Project4.rb
+++ Project4.rb
@@ -4,17 +4,43 @@ class Pair
     @value2 = value2
   end
 
+  def self.null
+    return Pair.new(nil, nil)
+  end
+
   def car
-    @value1
+    return @value1
   end
 
   def cdr
-    @value2
+    return @value2
+  end
+
+  def null?
+    return (@value1 == nil && @value2 == nil)
+  end
+
+  def list?
+    if (null?)
+      return true
+    end
+
+    if (@value2.class == Pair)
+      @value2.list?
+    else
+      return false
+    end
   end
 
   def to_s
     #TODO test if value is a pair
     "(#{@value1} . #{@value2})"
+  end
+end
+
+class NilList < Pair
+  def initialize
+    super(nil, nil)
   end
 end
 
