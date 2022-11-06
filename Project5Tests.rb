1d069b20143c0195ab8adda51354c25c1a0c0b63
Eric Foy <foy.8@wright.edu>
2022-11-06 18:18:17 -0500

    inverted compare method

--- Project5Tests.rb
+++ Project5Tests.rb
@@ -6,3 +6,13 @@ b.add(15)
 b.add(28)
 b.add(25)
 b.add(33)
+
+
+b = BST.new{ |a, b| if (a > b) then -1 elsif (a > b) then 1 else 0 end }
+b.add(24)
+b.add(13)
+b.add(10)
+b.add(15)
+b.add(28)
+b.add(25)
+b.add(33)
