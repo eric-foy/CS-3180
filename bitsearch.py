1c1ed26141b36637bdfd505827d8237a09965b17
Eric Foy <foy.8@wright.edu>
2022-11-16 15:33:08 -0500

    now finds bits inbetween bytes

--- bitsearch.py
+++ bitsearch.py
@@ -9,17 +9,25 @@ def search_bits(filename, d):
         g.append(c)
         e.append(d)
 
-    print(g)
-    print(e)
+    print(list(map(hex, e)))
     f = open(filename, "r")
     l = f.read()
     f.close()
     c = 0
+    p = ''
     for i in l:
         for j in range(0, len(g)):
             if ((ord(i) & e[j] == e[j]) and (ord(i) & g[j] == 0)):
                 print(hex(c), "^", j)
+        if (p != ''):
+            a = (ord(p) & 0x0f) << 4
+            b = (ord(i) & 0xf0) >> 4
+            b = a | b
+            for j in range(0, len(g)):
+                if ((b & e[j] == e[j]) and (b & g[j] == 0)):
+                    print(hex(c - 1), "^", j + 4)
         c += 1
+        p = i
 
 def file_as_bits(filename):
     o = ""
