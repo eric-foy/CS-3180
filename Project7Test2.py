01e7e384c81a8930121aacbac597a62be0749493
Eric Foy <foy.8@wright.edu>
2022-12-01 10:13:44 -0500

    project7 test

---
+++ Project7Test2.py
@@ -0,0 +1,8 @@
+import Project7 as p7
+
+# use __iter__ and __next__ not for
+s1 = p7.Student(888,'Lisa','Simpson','Grad')
+s1.addClass('CS6500')
+s1.addClass('CEG100')
+print(s1)
+print(s1.classes)
